81755f8f88e3cb4382e41ba0eed5047b3554e51b
er-sahilbharti <sahil.884062@gmail.com>
2026-02-25 21:13:34 +0530

    some changes

--- backend/routes/chatRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { chat } = require('../controller/chatController');
-const router = express.Router();
-const fetchUser = require('../middleware/fetchUser');
-
-// Route 1: Chat with Gemini using: POST "/api/chat". Login required
-router.post('/chat', fetchUser, chat);
-
-module.exports = router;
